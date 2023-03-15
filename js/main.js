5753e91c4c711a709e87898a3e795318ce567074
aya aljalees <121544052+AyaAljalees@users.noreply.github.com>
2023-03-15 13:27:20 +0200

    add js

---
+++ js/main.js
@@ -0,0 +1,129 @@
+
+const inputSearch = document.querySelector("#search-input");
+const submitBtn = document.querySelector("#search-btn");
+const Name = document.querySelector(".mainName");
+const result = document.querySelector(".anime-result");
+const anime = document.querySelector("#anime");
+const animeDetails = document.querySelector(".anime-details");
+const closeAnime = document.querySelector("#gif-close-btn");
+const mainTitle = document.querySelector(".mainTi");
+const divDetails = document.querySelector(".gif-details-content");
+
+
+inputSearch.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        submitBtn.click();
+    }
+});
+
+//fetch function
+const fetch = (method, url, cb) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            cb(JSON.parse(xhr.responseText))
+        }
+    }
+    xhr.open(method, url);
+    xhr.send();
+}
+
+window.onload = () => {
+    inputSearch.value = `naurto`;
+    submitBtn.click();
+};
+
+
+submitBtn.addEventListener('click', () => {
+    anime.innerHTML = '';
+    const animeName = inputSearch.value;
+    const url1 = `https://kitsu.io/api/edge/anime?filter%5Btext%5D=${animeName}`
+
+    const mainFun = (obj1) => {
+        if (obj1.data.length !== 0) {
+            obj1.data.forEach(e => {
+                const card = document.createElement('div');
+                card.classList.add('anime-item');
+                anime.appendChild(card);
+
+                const imgDiv = document.createElement('div');
+                imgDiv.classList.add('anime-img');
+                card.appendChild(imgDiv);
+
+                const image = document.createElement('img');
+                image.src = e.attributes.posterImage.original;
+                imgDiv.appendChild(image);
+
+                const titleContainer = document.createElement('div');
+                titleContainer.classList.add('anime-name');
+                card.appendChild(titleContainer);
+
+                const title = document.createElement('h3');
+                title.classList.add('mainAnime')
+                title.textContent = e.attributes.titles.en_jp
+                titleContainer.appendChild(title);
+
+                const InfoDiv = document.createElement('div');
+                InfoDiv.classList.add('synopsis');
+                card.appendChild(InfoDiv);
+
+                const Info = document.createElement('blockquote');
+                Info.textContent = e.attributes.synopsis
+                InfoDiv.appendChild(Info);
+
+                const btnDiv = document.createElement('div');
+                btnDiv.classList.add('anime-btn');
+                card.appendChild(btnDiv);
+
+                const btn = document.createElement('button');
+                btn.classList.add('gifs-btn');
+                btn.textContent = " GIF!"
+                btnDiv.appendChild(btn);
+            });
+
+        }
+        else {
+            anime.innerHTML = '';
+            const imageNotFound = document.createElement('div');
+            imageNotFound.classList.add('notFound');
+            const defaultImage = document.createElement('img');
+            defaultImage.src = './imgs/error-404.png';
+            defaultImage.setAttribute('width', '150px');
+            imageNotFound.textContent = "We didn't find any Anime,try again!"
+            result.appendChild(imageNotFound.appendChild(defaultImage));
+
+        }
+    }
+    fetch('GET', url1, mainFun)
+})
+
+
+anime.addEventListener("click", () => {
+    divDetails.innerHTML = '';
+    const animeName = inputSearch.value;
+    const url2 = `https://api.giphy.com/v1/gifs/search?api_key=BEhmVfKdPW8J3qZKPhXMCyNszDlbEci4&q=${animeName}` //images
+    animeDetails.style.display = 'block'
+    const displayImages = (obj2) => {
+        obj2.data.slice(-10).forEach(e => {
+            const GifContainer = document.createElement('div');
+            GifContainer.classList.add('anime-gif-img');
+            divDetails.appendChild(GifContainer);
+
+            const GifImage = document.createElement('img');
+            GifImage.src = e.images.downsized_medium.url;
+            GifContainer.appendChild(GifImage);
+        });
+
+    }
+    fetch('GET', url2, displayImages)
+});
+
+
+closeAnime.addEventListener("click", () => {
+    animeDetails.style.display = 'None'
+})
+
+
+
+
